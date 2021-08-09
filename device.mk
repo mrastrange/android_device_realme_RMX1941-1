552d6406b7e6d55000952bf9e1b76d6ef7528f0d
TheMalachite <eliasgheeraert@gmail.com>
2021-08-09 08:57:38 +0530

    RMX1941: Update DRM from RMX2185 A11
    * from sys_oplus_mssi_64_cn-user 11 RP1A.200720.011 1609743540423 release-keys
    
    Signed-off-by: P-Salik <peersalik16@gmail.com>

--- device.mk
+++ device.mk
@@ -93,7 +93,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.1 \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2 \
-    android.hardware.drm@1.2.vendor
+    android.hardware.drm@1.2.vendor \
+    android.hardware.drm@1.3-service.clearkey
 
 # FSTAB
 PRODUCT_COPY_FILES += \
