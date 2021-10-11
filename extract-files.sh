03152f44d785daf0a49c93080b49d4d5a8dc2183
Kshitij Gupta <kshitijgm@gmail.com>
2021-10-11 11:29:42 +0530

    RMX1941: Inject libcompiler_rt for WiFi blobs
    * Fixes missing symbol __subtf3
    
    Signed-off-by: P-Salik <peersalik16@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -56,6 +56,15 @@ if [ -z "$SRC" ]; then
 fi
 function blob_fixup() {
     case "${1}" in
+	    vendor/bin/hw/android.hardware.wifi@1.0-service-mediatek)
+            "${PATCHELF}" --add-needed "libcompiler_rt.so" "${2}"
+	    ;;
+            vendor/bin/hw/hostapd)
+	    "${PATCHELF}" --add-needed "libcompiler_rt.so" "${2}"
+            ;;
+	    vendor/bin/hw/wpa_supplicant)
+            "${PATCHELF}" --add-needed "libcompiler_rt.so" "${2}"
+            ;;
             vendor/lib/hw/android.hardware.audio@4.0-impl-mediatek.so)
             "${PATCHELF}" --replace-needed "android.hardware.audio.common@4.0-util.so" "android.hardware.audio.common@4.0-util-v28.so" "${2}"
             ;;
