0f1232b8522f3b9ca460bb8b8e9c7ccec1ff7451
imranpopz <imranpopz32@gmail.com>
2022-01-12 00:42:35 +0530

    RMX1941: Build vendor variant of libkeymaster3 from source
    Signed-off-by: P-Salik <peersalik16@gmail.com>

--- device.mk
+++ device.mk
@@ -91,6 +91,8 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
+    libkeymaster3.vendor \
+    libkeymaster3support.vendor \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
